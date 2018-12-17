4acb62a5109b99eee827b03ccb356c6eec992a73
EricR <rasliche@gmail.com>
2018-12-17 09:14:25 -0500

    register form and logic

--- public/js/register-form.js
+++ public/js/register-form.js
@@ -2,7 +2,21 @@ console.log("Imported register-form.js to the Register page.")
 
 new Vue({
     el: '#register-form',
-    data: {
-      checkedNames: []
+    data(){
+      return {
+        userData: {
+          email: '',
+          password: '',
+          passwordMatch: ''
+        },
+        formSubmitted: false,
+        formErrors: []
+      }
+    },
+    methods: {
+      submit() {
+        console.log(this.userData)
+        this.formSubmitted = true
+      }
     }
   })
