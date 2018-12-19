afa6697377f22c2e1b9e17c23e73efe6d89549ac
EricR <rasliche@gmail.com>
2018-12-19 15:48:57 -0500

    register with hashed password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcrypt')
+const _ = require('lodash')
+const { User } = require('../models/user')
+
 exports.getRegister = (req, res, next) => {
     res.render('auth/register', {
         pageTitle: "Register",
@@ -5,11 +9,27 @@ exports.getRegister = (req, res, next) => {
     })
 }
 
-exports.postRegister = (req, res, next) => {
+exports.postRegister = async (req, res, next) => {
+    const email = req.body.email
+    const password = req.body.password
+    const confirmPassword = req.body.confirmPassword
 
+    let user = await User.findOne({ email: email })
+    if (user) return res.redirect('/register')
+
+    const hashedPass = await bcrypt.hash(password, 10)
+    user = new User({
+        email: email,
+        password: hashedPass
+    })
+    await user.save()
+
+    res.redirect('/login')
 }
 
 exports.getLogin = (req, res, next) => {
+    const email = req.body.email
+
     res.render('auth/login', {
         pageTitle: "Login",
         loggedIn: req.session.loggedIn
@@ -17,9 +37,16 @@ exports.getLogin = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
+    console.log("req.body: ", req.body)
+    const email = req.body.email
+    const password = req.body.password
+    // req.session.loggedIn = true
+    res.redirect('/me')
+}
+
 exports.postLogout = (req, res, next) => {
     req.session.destroy(err => {
         if (err) console.log(err)
-    res.redirect('/')
+        res.redirect('/')
     })
 }
