a82ac53558ea8efd2035fc13d2a3ecacbad8b791
EricR <rasliche@gmail.com>
2018-12-20 18:18:45 -0500

    add shop routes

--- startup/routes.js
+++ startup/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const adminRoutes = require('../routes/admin')
 const usersRoutes = require('../routes/users')
+const shopsRoutes = require('../routes/shops')
 const trainingRoutes = require('../routes/training')
 const communityRoutes = require('../routes/community')
 const authRoutes = require('../routes/auth')
@@ -19,6 +20,7 @@ module.exports = function(app) {
     })
 
     app.use('/', usersRoutes)
+    app.use('/shops', shopsRoutes)
     app.use('/training', trainingRoutes)
     app.use('/admin', [auth, admin], adminRoutes)
     app.use('/community', communityRoutes)
