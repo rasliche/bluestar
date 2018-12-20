3acb72f81680f530c0b095a9b6d208e08f0685a5
EricR <rasliche@gmail.com>
2018-12-20 09:26:06 -0500

    Remove double declaration

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,6 @@ const router = express.Router()
 const usersController = require('../controllers/users')
 const auth = require('../middleware/auth')
 
-const auth = require('../middleware/auth')
-
 // GET /me
 router.get('/me', auth, usersController.getMe)
 
