c99bfcd28f399a9f9751fb60290c3c058a0bb28b
Eric R <rasliche@gmail.com>
2018-12-25 21:18:49 -0500

    set error flash messages in login

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,14 +41,23 @@ exports.postRegister = async (req, res, next) => {
 }
 
 exports.getLogin = (req, res, next) => {
+    let errorMessage = req.flash('error')
+    if (errorMessage.length > 0) {
+        errorMessage = errorMessage[0]
+    } else {
+        errorMessage = null
+    }
+
     let successMessage = req.flash('success')
     if (successMessage.length > 0) {
         successMessage = successMessage[0]
     } else {
         successMessage = null
     }
+
     res.render('auth/login', {
         pageTitle: "Login",
+        errorMessage: errorMessage,
         successMessage: successMessage
         })
 }
