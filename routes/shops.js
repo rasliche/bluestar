02219cf22e787425f562a9ee63122388d5e30f7e
Eric R <rasliche@gmail.com>
2019-01-03 12:21:36 -0500

    crud routing

--- routes/shops.js
+++ routes/shops.js
@@ -3,25 +3,26 @@ const router = express.Router()
 
 const shopsController = require('../controllers/shops')
 
+// RESTful Routes
 // GET /shops
 router.get('/', shopsController.getShops)
-
-// GET /shops/:shopId
-router.get('/:shopId', shopsController.getShop)
 
 // GET /shops/new
 router.get('/new', shopsController.getNewShop)
 
-// POST /shops
-router.post('/', shopsController.postShops)
-
 // GET /shops/:shopId/edit
-router.get('/:shopId/edit')
+router.get('/:shopId/edit', shopsController.getEditShop)
 
-// PUT /shops/:shopId
-router.put('/:shopId')
+// GET /shops/:shopId
+router.get('/:shopId', shopsController.getShop)
 
 // DELETE /shops/:shopId/delete
-router.delete('/:shopId/delete')
+router.post('/:shopId/delete', shopsController.deleteShop)
+
+// POST /shops/:shopId
+router.post('/:shopId', shopsController.postUpdateShop)
+
+// POST /shops
+router.post('/', shopsController.postShops)
 
 module.exports = router
