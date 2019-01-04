8f653e6bf0bb44562261851859d999fae032f4c4
Eric R <rasliche@gmail.com>
2019-01-04 14:54:07 -0500

    newUrl Parser for mongoose connect

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 const config = require('config')
 
 module.exports = function() {
-    mongoose.connect(config.get('db'))
+    mongoose.connect(config.get('db'), {
+        useNewUrlParser: true
+    })
         .then(result => {
             console.log(`Connected to MongoDB at ${config.get('db')}`)
         })
