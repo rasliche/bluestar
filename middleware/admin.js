4a3445f276a81831ffcbf5c272e9c122bb39ce83
Eric R <rasliche@gmail.com>
2019-01-10 08:31:28 -0500

    Quick flash error for unauthorized access

--- middleware/admin.js
+++ middleware/admin.js
@@ -2,5 +2,6 @@ module.exports = (req, res, next) => {
     if (req.session.user.isAdmin) {
         return next()
     }
+    req.flash('error', 'Not authorized to view the requested page.')
     res.redirect('/login')
 }
