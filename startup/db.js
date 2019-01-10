9746a450cd95988ced8004fa6b390a1e336d1728
Eric R <rasliche@gmail.com>
2019-01-10 17:05:12 -0500

    deploy

--- startup/db.js
+++ startup/db.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 const config = require('config')
 
 module.exports = function() {
-    mongoose.connect(config.get('db'), {
+    mongoose.connect(process.env.db, {
         useNewUrlParser: true
     })
         .then(result => {
-            console.log(`Connected to MongoDB at ${config.get('db')}`)
+            console.log(`Connected to MongoDB at ${process.env.db}`)
         })
         .catch(err => console.error(err))
 }
