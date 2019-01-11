cabe9ac144aa2fb8b13e6cf5a0beb83f864615d0
Eric R <rasliche@gmail.com>
2019-01-11 16:57:21 -0500

    cleanup

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,20 +5,16 @@ exports.getAdminIndex = async (req, res, next) => {
     // select users with most recently completed modules
     const users = await User.find().select(["-password"])
     const shops = await Shop.find()
-    console.log(users)
     res.render('admin/index', {
         pageTitle: 'Admin Panel',
         users: users,
-        shops: shops,
-        // user: req.session.user
-        // shops: Shop.fetchAll()
+        shops: shops
     })
 }
 
 exports.postAddShop = async (req, res, next) => {
     let shop = await Shop.findOne({ name: req.body.name })
     if (shop) return res.redirect('/admin')
-
     shop = new Shop({
         name: req.body.name,
         password: req.body.password
