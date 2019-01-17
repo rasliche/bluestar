6cf2b27bc6d3ef3f571e8311c6ef01b3048bd43c
Eric R <rasliche@gmail.com>
2019-01-17 17:05:01 -0500

    route guards

--- routes/shops.js
+++ routes/shops.js
@@ -2,27 +2,30 @@ const express = require('express')
 const router = express.Router()
 
 const shopsController = require('../controllers/shops')
+const auth = require('../middleware/auth')
+const admin = require('../middleware/admin')
+const canManage = require('../middleware/can-manage')
 
 // RESTful Routes
 // GET /shops
 router.get('/', shopsController.getShops)
 
 // GET /shops/new
-router.get('/new', shopsController.getNewShop)
+router.get('/new', [auth, admin], shopsController.getNewShop)
 
 // GET /shops/:shopId/edit
-router.get('/:shopId/edit', shopsController.getEditShop)
+router.get('/:shopId/edit', [auth, canManage], shopsController.getEditShop)
 
 // GET /shops/:shopId
 router.get('/:shopId', shopsController.getShop)
 
 // DELETE /shops/:shopId/delete
-router.post('/:shopId/delete', shopsController.deleteShop)
+router.post('/:shopId/delete', [auth, admin], shopsController.deleteShop)
 
 // POST /shops/:shopId
-router.post('/:shopId', shopsController.postUpdateShop)
+router.post('/:shopId', [auth, admin], shopsController.postUpdateShop)
 
 // POST /shops
-router.post('/', shopsController.postShops)
+router.post('/', [auth, admin], shopsController.postShops)
 
 module.exports = router
