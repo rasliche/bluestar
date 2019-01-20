14a83b4463a64391c883b83f3213d5e4f4da9d1a
Eric R <rasliche@gmail.com>
2019-01-20 17:21:04 -0500

    quiz model

---
+++ models/quiz.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const Joi = require('joi')
+
+const quizSchema = new mongoose.Schema({
+    // TODO: Shape data better (min lengths, max lengths)
+    questions: [
+        {
+            text: String,
+            wrongAnswers: [
+                { type: String }
+            ],
+            correctAnswers: [
+                { type: String }
+            ]
+        }
+    ],
+    isActive: {
+        type: Boolean,
+        default: false
+    },
+    createdDate: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    } 
+})
+
+function validateQuiz(quiz) {
+    const schema = {
+        
+    }
+
+    return Joi.validate(quiz, schema)
+}
+
+const Quiz = mongoose.model('Quiz', quizSchema)
+
+module.exports.Quiz = Quiz
+module.exports.validateQuiz = validateQuiz
