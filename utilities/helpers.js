b4cbbf6ca874f841c8aaabc9f49f41f5a24fd2cd
Eric R <rasliche@gmail.com>
2019-01-31 15:54:30 -0500

    helper file

--- utilities/helpers.js
+++ utilities/helpers.js
@@ -18,7 +18,7 @@ exports.dump = (obj) => JSON.stringify(obj, null, 2);
 // exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
 // Some details about the site
-// exports.siteName = `Now That's Delicious!`;
+exports.siteName = `Blue Star`;
 
 // exports.menu = [
 //   { slug: '/stores', title: 'Stores', icon: 'store', },
