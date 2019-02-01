28991b2c9be106d33e3d65e5cd1a3321426dca97
Eric R <rasliche@gmail.com>
2019-01-31 22:21:39 -0500

    admin stuff

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,11 @@
 exports.homePage = (req, res, next) => {
     res.send(`Admin Homepage`)
 }
+
+exports.addLesson = (req, res, next) => {
+    res.render(`editLesson`, { title: `Add Lesson` })
+}
+
+exports.addShop = (req, res, next) => {
+    res.render(`editShop`, { title: `Add Shop` })
+}
