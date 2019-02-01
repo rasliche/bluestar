373a2e6b44c130cf6dbb5aff6c44700e555559a6
Eric R <rasliche@gmail.com>
2019-01-31 22:22:05 -0500

    stuff

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ const trainingController = require('../controllers/trainingController')
 const adminController = require('../controllers/adminController')
 
 router.get('/', trainingController.homePage)
-router.get('/add', trainingController.addLesson)
 
 router.get('/admin', adminController.homePage)
+router.get('/admin/shop/add', adminController.addShop)
+router.get('/admin/shop/add', adminController.addShop)
 
 module.exports = router
