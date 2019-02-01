b6ee10a22ef3585ffb21834464f65da8d9d29d67
Eric R <rasliche@gmail.com>
2019-01-31 21:25:41 -0500

    how the hell do I fix this

--- startup/middleware.js
+++ startup/middleware.js
@@ -44,7 +44,7 @@ module.exports = function(app) {
     
     app.use((req, res, next) => {
         res.locals.csrfToken = req.csrfToken()
-        res.locals.loggedInUser = req.user || false
+        res.locals.user = req.user || null
         next()
     })
     
