f8d54b64079aa8b734b1b28af1954b1ffbbbb877
Eric R <rasliche@gmail.com>
2019-01-31 22:02:10 -0500

    morgan and helpers

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const config = require('config')
+const morgan = require('morgan')
 const express = require('express')
 const app = express()
 const routes = require('./routes/index')
 const errorController = require('./controllers/errorController')
+const helpers = require('./helpers')
 
 // configuration
 app.set('view engine', 'pug')
 app.set('views', './views')
 app.use(express.static('public'))
 
+if (app.get('env') === 'development') {
+    app.use(morgan('dev'))
+}
+
+// startup
+require('./startup/middleware')(app)
+require('./startup/db')()
+
+app.use((req, res, next) => {
+    // res.locals.csrfToken = req.csrfToken()
+    res.locals.h = helpers
+    res.locals.user = req.user || null
+    next()
+})
+
 // Use our routes
 app.use(routes)
 app.use(errorController.catchErrors)
-
-// startup
-require('./startup/logging')()
-require('./startup/middleware')(app)
-require('./startup/db')()
-require('./startup/config')()
-require('./startup/prod')(app)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}.`))
--- startup/middleware.js
+++ startup/middleware.js
@@ -5,6 +5,7 @@ const session = require('express-session')
 const MongoDBStore = require('connect-mongodb-session')(session)
 const csrf = require('csurf')
 const flash = require('connect-flash')
+const helpers = require('../helpers')
 
 const csrfProtection = csrf()
 
@@ -19,43 +20,26 @@ const store = new MongoDBStore({
 
 module.exports = function(app) {
     app.use(express.urlencoded({ extended: true }))
-    app.use(session({
-        secret: config.get('session_secret'),
-        resave: false,
-        saveUninitialized: false,
-        store: store
-    }))
-    app.use(csrfProtection)
+    // app.use(session({
+    //     secret: config.get('session_secret'),
+    //     resave: false,
+    //     saveUninitialized: false,
+    //     store: store
+    // }))
+    // app.use(csrfProtection)
     app.use(flash())
     
-    app.use(async (req, res, next) => {
-        try {
-            if (!req.session.user) return next()
-            // Find the current logged in user
-            const user = await User.findById(req.session.user)
-                .populate('shops', 'name')
-                .select('-password')
-            req.user = user
-            next()
-        } catch (err) {
-            throw new Error(err)
-        }
-    })
-    
-    app.use((req, res, next) => {
-        res.locals.csrfToken = req.csrfToken()
-        res.locals.user = req.user || null
-        next()
-    })
-    
-    // if (app.get('env') === 'development') {
-        console.log(`app: ${app.get('env')}`)
-        app.use(morgan('dev'))
-        console.log('Morgan enabled...')
-
-        app.use((req, res, next) => {
-            console.log(req.session)
-            next()
-        })
-    // }
+    // app.use(async (req, res, next) => {
+    //     try {
+    //         if (!req.session.user) return next()
+    //         // Find the current logged in user
+    //         const user = await User.findById(req.session.user)
+    //             .populate('shops', 'name')
+    //             .select('-password')
+    //         req.user = user
+    //         next()
+    //     } catch (err) {
+    //         throw new Error(err)
+    //     }
+    // })
 }
